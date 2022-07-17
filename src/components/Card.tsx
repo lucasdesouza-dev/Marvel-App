8ba573842b1169887f3af6bfd5ed662032ad8c3d
lusousouza <luca.s.ouza@hotmail.com>
2022-07-16 21:12:13 -0300

    ajustando carde e verificacao de descricao

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,20 +4,28 @@ interface Props {
 }
 
 const Card: React.FC<Props> = ({ personagem }) => {
+  const verificar = (parametro: string | undefined) => {
+    return parametro === undefined || parametro === ""
+      ? `The character ${personagem.name} has no description assigned.`
+      : parametro;
+  };
   return (
-    <div className=" card  max-w-md bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700  m-2">
+    <div className=" card  p-4 max-w-md bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700  m-2">
       <div className=" flex  justify-center ">
-        <span className="text-xl font-bold ">{personagem.name}</span>{" "}
+        <span className="text-xl font-bold mb-4">{personagem.name}</span>{" "}
       </div>
+      <picture>
+        <source />
+        <img
+          className="rounded-lg w-[414px] h-[414px]"
+          src={`${personagem.thumbnail.path}.${personagem.thumbnail.extension}`}
+          alt={`Imagem ${personagem.name} `}
+        />
+      </picture>
 
-      <img
-        className="rounded-t-lg "
-        src={`${personagem.thumbnail.path}.${personagem.thumbnail.extension}`}
-        alt={`Imagem ${personagem.name} `}
-      />
       <div className="p-5   m-2 ">
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          {personagem.description}
+          {verificar(personagem.description)}
         </p>
       </div>
     </div>
--- src/components/Personagens.tsx
+++ src/components/Personagens.tsx
@@ -8,16 +8,18 @@ interface Props {
 
 const Personagens: React.FC<Props> = ({ data }) => {
   return (
-    <div className="flex flex-col ">
-      <div className=" text-6xl m-6   font-bold flex justify-center ">
-        Personagens
+    <div className="w-full flex justify-center items-center">
+      <div className="flex flex-col w-[70%] ">
+        <div className=" text-6xl m-6   font-bold flex justify-center ">
+          Personagens
+        </div>
+        <ul className="flex flex-wrap justify-center justify-items-center">
+          {data != null &&
+            data.map((personagem) => {
+              return <Card key={personagem.id} personagem={personagem} />;
+            })}
+        </ul>
       </div>
-      <ul className="flex flex-wrap justify-center justify-items-center">
-        {data != null &&
-          data.map((personagem) => {
-            return <Card key={personagem.id} personagem={personagem} />;
-          })}
-      </ul>
     </div>
   );
 };
